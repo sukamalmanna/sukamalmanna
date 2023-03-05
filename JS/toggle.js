9eddb626b9c3225996bc998784f7e0867d6fcce9
Sukamal Manna <107705327+sukamalmanna@users.noreply.github.com>
2023-03-05 18:10:29 +0530

    Add files via upload

---
+++ JS/toggle.js
@@ -0,0 +1,10 @@
+
+// Coding By Sukamal Manna
+
+
+function toggleMenu(){
+    var menuToggle = document.querySelector('.toggle');
+    var menu = document.querySelector('.menu');
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('active');
+}
